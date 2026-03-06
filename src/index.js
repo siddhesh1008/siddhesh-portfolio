716f1c228440d72f00f2b77d81e4cd8b1718bb6c
siddhesh1008 <siddhesh2152@gmail.com>
2026-03-06 12:48:18 +0100

    new push

--- src/index.js
+++ src/index.js
@@ -4,16 +4,3 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-```
-
-Save it with Ctrl+S.
-
-**Fix 2: Check App.js**
-
-Click on App.js tab at the top. Does it have the portfolio code in it or is it still the default React boilerplate? Tell me what you see.
-
-**Fix 3: Confirm which GitHub account**
-
-In the terminal at the bottom run:
-```
-git remote -v
